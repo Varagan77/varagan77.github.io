86994e835c47ca7d07bede07deec4cecbe6c3dbc
Varagan77 <johanneswillemkotze@gmail.com>
2025-05-08 17:36:39 +0200

    fixed theme toggle and appropriate icons used

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const toggleButton = document.getElementById('theme-toggle');
+const toggleInput = document.getElementById('theme-toggle');
 const body = document.body;
 
 body.classList.add('light-mode');
 
-toggleButton.addEventListener('click', () => {
-    if (body.classList.contains('light-mode')) {
+toggleInput.addEventListener('change', () => {
+    if (toggleInput.checked) {
         body.classList.replace('light-mode', 'dark-mode');
-        toggleButton.textContent = 'Switch to Light Mode';
     } else {
         body.classList.replace('dark-mode', 'light-mode');
-        toggleButton.textContent = 'Switch to Dark Mode';
     }
 });
